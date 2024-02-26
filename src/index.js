d341c1410908ae1ae3caf24c956686cd1bcf9268
TheXro <ravyyadav97@gmail.com>
2024-02-26 12:49:30 +0530

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser');
+
+
+
 
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
+    
     const app = express();
     app.get('/', (req, res) => {
         res.send('Hello World!');
     });
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, () => {
         console.log(`Example app listening at http://localhost:${PORT}`);
